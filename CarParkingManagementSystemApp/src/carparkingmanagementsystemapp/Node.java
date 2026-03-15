9f122d67208dab2b08c71bdad5094088d86ccfb0
SianEAH <149701097+SianEAH@users.noreply.github.com>
2026-03-15 13:17:08 +0000

    Worked on Node class

--- CarParkingManagementSystemApp/src/carparkingmanagementsystemapp/Node.java
+++ CarParkingManagementSystemApp/src/carparkingmanagementsystemapp/Node.java
@@ -10,12 +10,43 @@ package carparkingmanagementsystemapp;
  */
 public class Node {
     //data members
+    protected CarParkingSpace space; //CarParkingSpace Object for my SLL
+    protected Node next;
     
     //constructor
+    //basic 
+    public Node() {
+        
+    }
+    
+    //other constructor
+    public Node(CarParkingSpace space, Node next) {
+        this.space = space;
+        this.next = next;
+    }
     
     //setters
+    public void setSpace(CarParkingSpace space) {
+        this.space = space;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
     
     //getters
+    public CarParkingSpace getSpace() {
+        return space;
+    }
+
+    public Node getNext() {
+        return next;
+    }
     
     //methods
+    //toString method
+    @Override
+    public String toString() {
+        return space.toString();
+    }
 }
